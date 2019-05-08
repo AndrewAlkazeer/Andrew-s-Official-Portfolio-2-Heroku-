7cb77fc12b4622acc6f227f165f2418a1f4121f0
AndrewAlkazeer <35503610+AndrewAlkazeer@users.noreply.github.com>
2019-05-08 01:20:36 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+    
+app.get('/contact', (req, res)=>{
+
+    res.sendFile(__dirname + '/public/sayHello/sayHello.html')
+});
+
+app.get('/home', (req, res)=>{
+
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+const server = app.listen(PORT, ()=>{
+    console.log('Server is listening on port 3000');
+})
